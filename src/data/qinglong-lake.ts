7fff6a2622d1ad1699c41250926b0b6d99e49071
icelin <1256422744@qq.com>
2026-06-09 22:05:24 +0800

    Add Qinglong Lake photo section

---
+++ src/data/qinglong-lake.ts
@@ -0,0 +1,10 @@
+export type QinglongLakePhoto = {
+  title: string;
+  pubDate: Date;
+  image: string;
+  alt: string;
+  caption?: string;
+  tags?: string[];
+};
+
+export const qinglongLakePhotos: QinglongLakePhoto[] = [];
